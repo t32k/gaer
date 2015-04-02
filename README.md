86324d9f5da02b159b3ace963ad5871ae44fd7b0
t32k <ijok.ijok@gmail.com>
2015-04-01 19:57:32 -0700

    Add chalk module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GAETR
+
+Measurement Protocol
--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+'use strict';
+
+var _ = require('underscore');
+var program = require('commander');
+
+program
+  .version(require('../package.json').version)
+  .usage('[options] <file ...>')
+  .parse(process.argv);
+
+if (!program.args.length) {
+  console.log(chalk.red('\n No input file specified.'));
+  program.help();
+}
