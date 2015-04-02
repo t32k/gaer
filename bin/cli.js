61df2a366ae14d7dcc7fc7bb06dcf5e8e878a4c8
t32k <ijok.ijok@gmail.com>
2015-04-02 03:01:06 -0700

    Add send data part

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 'use strict';
-
+var fs = require('fs');
 var _ = require('underscore');
 var chalk = require('chalk');
 var request = require('request');
 var program = require('commander');
+
+var util = require('../lib/util');
 
 var TOOL_NAME = 'GAER';
 var GA_URL_ENDPOINT = 'https://ssl.google-analytics.com/collect';
@@ -14,17 +16,21 @@ var GA_CLIENT_ID = Math.round(2147483647 * Math.random());
 var GA_HIT_TYPE = 'event';
 var GA_EVENT_CATEGORY = TOOL_NAME;
 
+
 program
   .version(require('../package.json').version)
   .usage('[options] <JSON>')
   .option('-t, --tid [ID]', 'set your Google Analitics tracking ID')
   .option('-a, --action [Name]', 'set your Action report name')
+  .option('-s, --stdin', 'Input JSON using stdin')
   .parse(process.argv);
+
 
 if (!program.args.length) {
   console.log(chalk.red('\n No input file specified.'));
   program.help();
 }
+
 
 // Google Analytics Tracking ID (EX: 'UA-12345-6')
 var gaTrackingId = process.env.GA_TID || program.tid || '';
@@ -32,42 +38,59 @@ gaTrackingId = gaTrackingId.toUpperCase();
 
 if (gaTrackingId === '') {
   console.log(chalk.red('`tid` option is required.'));
-  return false;
+  process.exit();
 } else if (gaTrackingId.indexOf('UA-') !== 0) {
   console.log(chalk.red('`tid` option is invalid.'));
-  return false;
+  process.exit();
 }
+
 
 // GA Event Action Name
 if (!program.action){
   console.log(chalk.red('`action` option is required.'));
-  return false;
+  process.exit();
 }
 var gaEventAction = program.action;
 
-var gaEvenLabel = 'Selecotors';
-var gaEventValue = 80;
 
-var options = {
-  url: GA_URL_ENDPOINT,
-  headers: { 'User-Agent': TOOL_NAME },
-  form: {
-    v: GA_VERSION,
-    tid: gaTrackingId,
-    cid: GA_CLIENT_ID,
-    t: GA_HIT_TYPE,
-    ec: GA_EVENT_CATEGORY ,
-    ea: gaEventAction,
-    el: gaEvenLabel,
-    ev: gaEventValue
+// GA data
+var jsonData = '';
+
+if (util.isFile(program.args[0])) {
+  var jsonString = fs.readFileSync(program.args[0], {
+    encoding: 'utf8'
+  });
+  try {
+    jsonData = JSON.parse(jsonString);
+  } catch (e) {
+    throw e;
   }
-};
-
-// request.post(options, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       console.log(chalk.green('Success!'));
-//     } else {
-//       console.log(chalk.red('Error: ', error));
-//     }
-//   }
-// );
+} else if (_.isObject(program.args[0])) {
+  jsonData = program.args[0];
+}
+_.each(jsonData, function(key, value){
+  var gaEvenLabel = key;
+  var gaEventValue = value;
+  var options = {
+    url: GA_URL_ENDPOINT,
+    headers: { 'User-Agent': TOOL_NAME },
+    form: {
+      v: GA_VERSION,
+      tid: gaTrackingId,
+      cid: GA_CLIENT_ID,
+      t: GA_HIT_TYPE,
+      ec: GA_EVENT_CATEGORY ,
+      ea: gaEventAction,
+      el: gaEvenLabel,
+      ev: gaEventValue
+    }
+  };
+  request.post(options, function (error, response) {
+      if (!error && response.statusCode == 200) {
+        console.log(chalk.green('Success!'));
+      } else {
+        console.log(chalk.red('Error: ', error));
+      }
+    }
+  );
+});
