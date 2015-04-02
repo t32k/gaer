436efdbad5f28c1fb470c31d69b6b654993a0160
t32k <ijok.ijok@gmail.com>
2015-04-02 01:38:17 -0700

    Add set id, set action

--- bin/cli.js
+++ bin/cli.js
@@ -7,14 +7,18 @@ var chalk = require('chalk');
 var request = require('request');
 var program = require('commander');
 
+var TOOL_NAME = 'GAER';
+var GA_URL_ENDPOINT = 'https://ssl.google-analytics.com/collect';
 var GA_VERSION = 1;
 var GA_CLIENT_ID = Math.round(2147483647 * Math.random());
 var GA_HIT_TYPE = 'event';
-var GA_EVENT_CATEGORY = 'GAER';
+var GA_EVENT_CATEGORY = TOOL_NAME;
 
 program
   .version(require('../package.json').version)
-  .usage('<GA Tracking ID> <Report Name> <JSON>')
+  .usage('[options] <JSON>')
+  .option('-t, --tid [ID]', 'set your Google Analitics tracking ID')
+  .option('-a, --action [Name]', 'set your Action report name')
   .parse(process.argv);
 
 if (!program.args.length) {
@@ -22,14 +26,31 @@ if (!program.args.length) {
   program.help();
 }
 
-var gaTrackingId = 'UA-2317436-26';
-var gaEventAction = 'StyleStas';
+// Google Analytics Tracking ID (EX: 'UA-12345-6')
+var gaTrackingId = process.env.GA_TID || program.tid || '';
+gaTrackingId = gaTrackingId.toUpperCase();
+
+if (gaTrackingId === '') {
+  console.log(chalk.red('`tid` option is required.'));
+  return false;
+} else if (gaTrackingId.indexOf('UA-') !== 0) {
+  console.log(chalk.red('`tid` option is invalid.'));
+  return false;
+}
+
+// GA Event Action Name
+if (!program.action){
+  console.log(chalk.red('`action` option is required.'));
+  return false;
+}
+var gaEventAction = program.action;
+
 var gaEvenLabel = 'Selecotors';
 var gaEventValue = 80;
 
 var options = {
-  url: 'https://ssl.google-analytics.com/collect',
-  headers: { 'User-Agent': 'GAER' },
+  url: GA_URL_ENDPOINT,
+  headers: { 'User-Agent': TOOL_NAME },
   form: {
     v: GA_VERSION,
     tid: gaTrackingId,
@@ -42,11 +63,11 @@ var options = {
   }
 };
 
-request.post(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(chalk.green('Success!'));
-    } else {
-      console.log(chalk.red('Error: ', error));
-    }
-  }
-);
+// request.post(options, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       console.log(chalk.green('Success!'));
+//     } else {
+//       console.log(chalk.red('Error: ', error));
+//     }
+//   }
+// );
