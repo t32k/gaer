ee6d5094db0dcb08154b8d6c419dbeb1f2f3da2e
t32k <ijok.ijok@gmail.com>
2015-04-05 05:10:00 -0700

    Add tests

--- test/spec.js
+++ test/spec.js
@@ -1,15 +1,29 @@
 var util = require('../lib/util.js');
-var assert = require('chai').assert;
+var Validator = require('../lib/validator');
+var expect = require('chai').expect;
 
 describe('Utility functions', function() {
   describe('isFile()', function () {
     it('should return true if specified path is a file', function() {
       var result = util.isFile('test/fixture/single.json');
-      assert.equal(result, true);
+      expect(result).to.be.true;
     });
     it('should return false if specified path is not a file', function() {
       var result = util.isFile('test/fixture/not_exist.json');
-      assert.equal(result, false);
+      expect(result).to.be.false;
     });
+  });
+  describe('parseJSON()', function () {
+    it('should return object if it is JSON strings', function() {
+      var result = util.parseJSON('{"foo": 1}');
+      expect(result).to.be.ok;
+    });
+  });
+});
+
+describe('Validator class', function() {
+  it('should return object if arguments are every correct', function() {
+    var result = new Validator('UA-1234-56', 'foo', '{"foo": 1}').validate();
+    expect(result).to.be.ok;
   });
 });
